5ed6040d957dafca978c71ae4b76f309627d2e2f
Alex Wenstrup <awenstrup@olin.edu>
2019-01-31 10:56:37 -0500

    Finished Day01 assignment, checked off

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -2,41 +2,88 @@ public class MyArrayList {
     private Cow[] elems;
     private int size;
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public MyArrayList() {
-        // TODO
+        elems = new Cow[10];
+        size = 0;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public MyArrayList(int capacity) {
-        // TODO
+        elems = new Cow[capacity];
+        size = 0;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public void add(Cow c) {
-        // TODO
+        if(size == elems.length) {
+            Cow[] temp = new Cow[elems.length * 2];
+            for(int i = 0; i < size; i++)
+                temp[i] = elems[i];
+            elems = temp;
+        }
+        elems[size] = c;
+        size++;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public int size() {
-        // TODO
-        return -1;
+        return size;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public Cow get(int index) {
-        // TODO
-        return null;
+        if(elems[index] == null)
+            throw new IndexOutOfBoundsException();
+        return elems[index];
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N)
     public Cow remove(int index) {
-        // TODO
-        return null;
+        if(elems[index] == null)
+            throw new IndexOutOfBoundsException();
+
+        Cow[] temp;
+
+        if(size < elems.length/4)
+            temp = new Cow[elems.length/2];
+        else
+            temp = new Cow[elems.length];
+
+        for(int i = 0; i < index; i++)
+            temp[i] = elems[i];
+
+        for(int i = index + 1; i < size; i++)
+            temp[i - 1] = elems[i];
+
+        Cow cow = elems[index];
+        elems = temp;
+        size--;
+
+        return cow;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N)
     public void add(int index, Cow c) {
-        // TODO
+        if(index > size)
+            throw new IndexOutOfBoundsException();
+
+        Cow[] temp;
+
+        if(size == elems.length)
+            temp = new Cow[elems.length * 2];
+        else
+            temp = new Cow[elems.length];
+
+        for(int i = 0; i < index; i++)
+            temp[i] = elems[i];
+
+        temp[index] = c;
+        size++;
+
+        for(int i = index + 1; i < size; i++)
+            temp[i] = elems[i-1];
+
+        elems = temp;
     }
 }
